b04bb54bc8f7e94d309e9b04dd557a32ef34796c
Mostafa <40375404+MostafaI@users.noreply.github.com>
2023-02-20 17:23:37 -0500

    fix docstring

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -7,13 +7,17 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 class CONFIG:
     batch_size = 64
-    num_epochs = 2
-    initial_learning_rate = 0.001
+    num_epochs = 12
+    initial_learning_rate = 0.003
     initial_weight_decay = 0
-
+    epoch_size = 782
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "decay_factor": 0.9,
+        "decay_epochs": 600,
+        "milestones": [3, 4 * epoch_size, 10 * epoch_size],
+        "lrs": [0.003, 0.0005, 0.00005],
     }
 
     optimizer_factory: Callable[
@@ -24,8 +28,4 @@ class CONFIG:
         weight_decay=CONFIG.initial_weight_decay,
     )
 
-    transforms = Compose(
-        [
-            ToTensor(),
-        ]
-    )
+    transforms = Compose([ToTensor(), Normalize(0, 1)])
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,25 +1,57 @@
 from typing import List
 
 from torch.optim.lr_scheduler import _LRScheduler
+import math
 
 
+# Old Adagrad
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    Creates a custom made learning rate schehuler based on manually giving the learning rates
+    """
+
+    # def __init__(self, optimizer, last_epoch=-1, decay_factor=0.1, decay_epochs=10, milestones=[]):
+    def __init__(
+        self,
+        optimizer,
+        last_epoch=-1,
+        lrs=[],
+        milestones=[],
+        decay_factor=0.1,
+        decay_epochs=10,
+    ):
         """
         Create a new scheduler.
-
-        Note to students: You can change the arguments to this constructor,
-        if you need to add new parameters.
+        Args:
+            optimizer (Optimizer): Wrapped optimizer.
+            last_epoch (int): The index of the last epoch. Default: -1.
+            decay_factor (float): Factor by which to decay the learning rate. Default: 0.1.
+            decay_epochs (int): Number of epochs after which to decay the learning rate. Default: 10.
 
         """
-        # ... Your Code Here ...
+        self.decay_factor = decay_factor
+        self.decay_epochs = decay_epochs
+        self.last_epoch = last_epoch
+        self.optimizer = optimizer
+        self.milestones = milestones
+        self.lrs = lrs
+        self.prev_lrs = []
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
-        # Note to students: You CANNOT change the arguments or return type of
-        # this function (because it is called internally by Torch)
-
-        # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        # Mostafa's Code
-        return [i for i in self.base_lrs]
+        """
+        updates the learning rate
+        """
+        current_lr = [
+            group["lr"] * self.decay_factor for group in self.optimizer.param_groups
+        ][0]
+        # if self.last_epoch ==0 : print('initial LR =',current_lr)
+        self.prev_lrs.append([group["lr"] for group in self.optimizer.param_groups][0])
+        for milestone, lr in zip(self.milestones, self.lrs):
+            if self.last_epoch == milestone:
+                # print(self.last_epoch, 'old LR =', current_lr)
+                # print('\tnew LR =', lr)
+                return [lr for group in self.optimizer.param_groups]
+        if self.last_epoch == 14 * 372:
+            plt.plot(self.prev_lrs)
+        return [group["lr"] for group in self.optimizer.param_groups]
