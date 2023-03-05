74b2e056ab85fafd105b598cbcb9a67b61f60861
Mostafa <40375404+MostafaI@users.noreply.github.com>
2023-03-05 14:51:20 -0500

    pool2

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -12,20 +12,21 @@ class Model(torch.nn.Module):
             in_channels=num_channels, out_channels=16, kernel_size=3
         )
         self.conv2 = torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
-        self.fc1 = torch.nn.Linear(1152, 128)
+        self.fc1 = torch.nn.Linear(1152, num_classes)
         self.fc2 = torch.nn.Linear(128, num_classes)
         self.pool = torch.nn.MaxPool2d(2, 2)
-        self.bn = torch.nn.BatchNorm2d(32)
+        self.bn1 = torch.nn.BatchNorm2d(16)
+        self.bn2 = torch.nn.BatchNorm2d(32)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         computes the output of the model
         """
-        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.bn1(self.conv1(x)))
         x = self.pool(x)
-        x = torch.relu(self.bn(self.conv2(x)))
+        x = torch.relu(self.bn2(self.conv2(x)))
         x = self.pool(x)
         x = x.view(x.size(0), -1)  # Flatten
-        x = torch.relu(self.fc1(x))
-        x = self.fc2(x)
+        # x = torch.relu(self.fc1(x))
+        x = self.fc1(x)
         return x
