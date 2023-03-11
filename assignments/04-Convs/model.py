03ba331dbea1e489ab3b31c692013636c89869c9
Mostafa <40375404+MostafaI@users.noreply.github.com>
2023-03-10 23:20:12 -0500

    ,,

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -9,15 +9,16 @@ class Model(torch.nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
         self.conv1 = torch.nn.Conv2d(
-            in_channels=num_channels, out_channels=16, kernel_size=3
+            in_channels=num_channels, out_channels=8, kernel_size=3
         )
-        self.conv2 = torch.nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3)
+        self.conv2 = torch.nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3)
         self.fc1 = torch.nn.Linear(576, num_classes)
         # self.fc2 = torch.nn.Linear(128, num_classes)
         self.pool = torch.nn.MaxPool2d(2, 2)
-        self.dropout = torch.nn.Dropout(0.5)
-        self.bn1 = torch.nn.BatchNorm2d(16)
+        self.dropout = torch.nn.Dropout(0.4)
+        self.bn1 = torch.nn.BatchNorm2d(8)
         self.bn2 = torch.nn.BatchNorm2d(16)
+        self.bn3 =  torch.nn.BatchNorm1d(576)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -29,6 +30,6 @@ class Model(torch.nn.Module):
         x = self.pool(x)
         x = self.dropout(x)
         x = x.view(x.size(0), -1)  # Flatten
-        x = self.fc1(x)
+        x = self.fc1(self.bn3(x))
         # x = self.fc2(torch.relu(x))
         return x
